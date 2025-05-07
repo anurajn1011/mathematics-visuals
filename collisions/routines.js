bc4bcb007a9cd6a2ac6c7c55540e20ddab685bc5
anurajn1011 <142703118+anurajn1011@users.noreply.github.com>
2025-05-06 20:52:55 -0500

    created elastic collision method, constructed by following the Laws of Conservation for Energy and Momentum. This theory will be outlined in a pdf readme doc.

--- collisions/routines.js
+++ collisions/routines.js
@@ -10,3 +10,23 @@ function wrap_around(center, radius, max_edge) {
     let position = ((center % region) + region) % region // handles negatives
     return position - radius; // return the respective coordinate of the new center
 }
+
+function are_colliding(ball1, ball2) {
+    /* 
+        Method for detecting whether two balls have collided. Returns a boolean.
+    */
+   if (ball1.position.dist(ball2) < ball1.radius + ball2.radius) {
+    return true;
+   }
+   return false;
+}
+
+function elastic_collision(mass1, mass2, velocity){
+    /* 
+        Method for computing the velocities of both balls post collision. 
+        We assume that mass2 is stationary, for which we can adjust the frame of reference for in the collision handling method.
+    */
+   let v1 = velocity * ((mass1 - mass2)/(mass1 + mass2));
+   let v2 = velocoty + v1;
+   return (v1, v2);
+}
